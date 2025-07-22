b96bb08603b9afbffd2d5d840f9715062c45f84b
AbdulrahmanAlnomrosi <a.alnomrosi@gmail.com>
2025-07-22 17:54:27 +0300

    added the minimum age requried for creating an account

--- Bank_Managment_Api_1.2/Dto/CreateAccountDto.cs
+++ Bank_Managment_Api_1.2/Dto/CreateAccountDto.cs
@@ -1,5 +1,5 @@
 using System.ComponentModel.DataAnnotations;
-
+using Bank_Managment_Api_1._2.validations;
 namespace Bank_Managment_Api_1._2.Dto;
 
 public record class CreateAccountDto
@@ -7,6 +7,7 @@ public record class CreateAccountDto
     [Required] [StringLength(35)] string HolderName,
     [Required] [StringLength(13)] string AssociatedPhoneNumber,
     decimal Balance,
+    [MinAge(18)]
     DateTime DateOfBirth,
     bool IsActive = true
 );
--- Bank_Managment_Api_1.2/obj/Debug/net8.0/Bank_Managment_Api_1.2.AssemblyInfo.cs
+++ Bank_Managment_Api_1.2/obj/Debug/net8.0/Bank_Managment_Api_1.2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank_Managment_Api_1.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1585a7915eb284c27dc0d7a8a514c4b190c665b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d77ce64c6cf52f2a291052b7c615cab500b11d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank_Managment_Api_1.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank_Managment_Api_1.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
